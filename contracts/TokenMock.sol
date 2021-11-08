de7f7d378853740ebb4f70c98b1e7365cd82be84
Eason <eason@Easons-MacBook.lan>
2021-11-08 17:24:50 +0800

    renew TokenMock.sol from JoltifyCoin

--- contracts/TokenMock.sol
+++ contracts/TokenMock.sol
@@ -1,32 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract JoltifyCoin is
-    ERC20,
-    ERC20Burnable,
-    Pausable,
-    AccessControl,
-    ERC20Capped
-{
-    uint256 public capOfToken = 21000000 * 10**decimals(); // set max supply
+contract JoltifyCoin is ERC20Capped, ERC20Burnable, Pausable, AccessControl  {
 
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE"); // 0x65d7a28e3265b37a6474929f336521b332c1681b933f6cb9f3376673440d862a
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE"); // 0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6
+    uint256 internal capOfToken = 21000000 * 10**decimals();
 
-    constructor()
-        ERC20("JoltifyCoin", "Jolt")
-        ERC20Capped(capOfToken)
-    {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC20("JoltifyCoin", "Jolt") ERC20Capped( capOfToken ) {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(PAUSER_ROLE, msg.sender);
         _setupRole(MINTER_ROLE, msg.sender);
-        // _mint(msg.sender, 210000 * 10 ** decimals());
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
@@ -37,14 +27,8 @@ contract JoltifyCoin is
         _unpause();
     }
 
-    function _mint(address account, uint256 amount)
-        internal
-        override(ERC20, ERC20Capped)
-    {
-        require(
-            ERC20.totalSupply() + amount <= cap(),
-            "ERC20Capped: cap exceeded"
-        );
+    function _mint(address account, uint256 amount) internal override(ERC20, ERC20Capped) {
+        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
         super._mint(account, amount);
     }
 
@@ -52,11 +36,12 @@ contract JoltifyCoin is
         _mint(to, amount);
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override whenNotPaused {
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
         super._beforeTokenTransfer(from, to, amount);
     }
+    
 }
