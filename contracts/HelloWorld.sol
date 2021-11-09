956d6873dcd79b59d3d763dd9f699ed3900b599c
Eason <eason@Easons-MacBook.local>
2021-11-09 16:50:29 +0800

    test file removed

--- contracts/HelloWorld.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.9;
-
-contract HelloWorld {
-    function sayHello() public pure returns(string memory) {
-        return "Hello World";
-    }
-}
--- test/HelloWorld.test.js
+++
@@ -1,8 +0,0 @@
-// const HelloWorld = artifacts.require('HelloWorld');
-// contract('HelloWorld', () => {
-//   it('Should echo "Hello World"', async () => {
-//     const helloWorld = await HelloWorld.new()
-//     const res = await helloWorld.sayHello()
-//     assert('Hello World'===res)
-//   })
-// })
